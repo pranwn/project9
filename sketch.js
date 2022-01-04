72228c4ee28b0308d8cc8170e64dfc923223fad6
pranwn <96743314+pranwn@users.noreply.github.com>
2022-01-03 18:12:22 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var ball
+function setup() {
+  createCanvas(400,400);
+  ball = createSprite(200,200,20,20);
+  ball.shapeColor = "blue"
+}
+
+function draw(){
+  background("yellow")
+  if (keyDown("right")) {
+    ball.velocityX = 5
+    background("red")
+  }
+  drawSprites()
+}
+
+
+
+
